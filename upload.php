477b51e467d77a031c9ca17b7b4a9625114005c8
Truzme_ <green57421@gmail.com>
2024-11-17 00:22:12 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 #$used_id = $_SESSION["used_id"];
-$upload_file = "images/" . basename($_FILES["images"]);
+$upload_file = "images/" . basename($_FILES["images"]["tmp_name"]);
 
 if (move_uploaded_file($_FILES["images"]["tmp_name"], $upload_file)) {
 	echo "Файл загружен на сервер!";

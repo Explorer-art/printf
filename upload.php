a1378e9a27197b3b5d9e584f7eb422805edaba1d
Truzme_ <green57421@gmail.com>
2024-11-17 00:53:56 +0300

    feat: add upload images

--- upload.php
+++ upload.php
@@ -1,9 +1,20 @@
 <?php
-#$used_id = $_SESSION["used_id"];
-$upload_file = "images/" . basename($_FILES["images"]["tmp_name"]);
+if (isset($_FILES["file"]) && $_FILES["file"]["error"] === UPLOAD_ERR_OK) {
+	$upload_dir = "images/";
 
-if (move_uploaded_file($_FILES["images"]["tmp_name"], $upload_file)) {
-	echo "Файл загружен на сервер!";
-} else {
-	echo "Ошибка! Возможная атака на сервер через загрузку файла.";
+	$file_tmp_path = $_FILES["file"]["tmp_name"];
+	$file_name = $_FILES["file"]["name"];
+	$file_size = $_FILES["file"]["size"];
+	$file_type = $_FILES["file"]["type"];
+	$file_path = $upload_dir . uniqid() . "_" . $file_name;
+
+	if (!is_dir($upload_dir)) {
+		mkdir($upload_dir, 0777, true);
+	}
+
+	if (move_uploaded_file($file_tmp_path, $file_path)) {
+		echo "Файл загружен на сервер!";
+	} else {
+		echo "Ошибка при загрузке файлов.";
+	}
 }

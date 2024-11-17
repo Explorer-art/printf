683605bb5b822cda1b06b4b9a14f5715a383de1c
Den <denstrilko@gmail.com>
2024-11-17 20:46:48 +0300

    Update edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -10,7 +10,7 @@ $user_id = $_SESSION['user_id'];
 $query =$connection->prepare("SELECT * FROM users WHERE id = ?");
 $query->execute([$user_id]);
 $user = $query->fetch();
-$email = $query->fetch();
+$new_email = trim(strip_tags(htmlspecialchars($_POST['new_email'])));
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
@@ -26,24 +26,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
 
-    $query = $connection->prepare("UPDATE users SET email = ? WHERE id = ?");
-    if ($query->execute([$email, $user_id])) {
-        echo "Данные успешно обновлены!";
-        header("Location: profile.php");
-        exit();
-    } else {
-        echo "Ошибка обновления данных";
-    }
+
+    $query = $connection->prepare("UPDATE users SET username = ? WHERE id = ?");
+        if($query->execute([$new_email, $user_id])){
+            echo "Электронная почта успешно обновлена";
+        }
+        else{
+            echo "Ошибка";
+        }
 }
     ?>;
 
 
 <h1>Редактировать профиль </h1>>
-<form method="post">
+<form action="edit_profile.php" method="post">
     <label for = "username">Имя: </label>
     <input type ="text" id = "username" name ="username" value ="<?php htmlspecialchars($user["username"]);?>" required>
-    <label for = "email">Почта: </label>
-    <input type ="text" id = "email" name ="email" value ="<?php htmlspecialchars($email["email"]);?>" required>
+    <label for = "email">Новая электронная почта</label>
+    <input type="email" id="email" name="email" required>
     <button type="submit">Сохранить изменения </button>
 </form>
 <a href = "profile.php">Назад к профилю</a>

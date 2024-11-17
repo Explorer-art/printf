64acda0ed1c2e955817424d0410287b98310c9ed
Truzme_ <green57421@gmail.com>
2024-11-17 19:42:06 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -5,18 +5,28 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 		$file_tmp_path = $_FILES["image"]["tmp_name"];
 		$file_name = $_FILES["image"]["name"];
-		$file_size = $_FILES["image"]["size"];
 		$file_type = $_FILES["image"]["type"];
-		$file_path = $upload_dir . uniqid() . "_" . $file_name;
+		$file_size = $_FILES["image"]["size"];
 
-		if (!is_dir($upload_dir)) {
-			mkdir($upload_dir, 0777, true);
-		}
+		if ($file_type == "image/png" or $file_type == "image/jpg" or $file_type == "image/jpeg") {
+			$user_id = $_SESSION["user_id"];
+			$file_path = $upload_dir . $user_id . "/" . uniqid() . "_" . $file_name;
 
-		if (move_uploaded_file($file_tmp_path, $file_path)) {
-			echo "Файл загружен на сервер!";
+			if (!is_dir($upload_dir)) {
+				mkdir($upload_dir, 0777, true);
+			}
+
+			if (!is_dir($upload_dir . $user_id)) {
+				mkdir($upload_dir . $user_id, 0777, true);
+			}
+
+			if (move_uploaded_file($file_tmp_path, $file_path)) {
+				echo "Файл загружен на сервер!";
+			} else {
+				echo "Ошибка при загрузке файлов.";
+			}
 		} else {
-			echo "Ошибка при загрузке файлов.";
+			echo "Ошибка! Неподдерживаемый тип файла.";
 		}
 	}
 } else {

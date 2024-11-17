08a5fc367e32b67b38c5aec7d6c618f72154c4a3
Truzme_ <green57421@gmail.com>
2024-11-17 10:53:19 +0300

    feat: add header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+	<div class="logo">PRINTF</div>
+	<nav>
+		<ul class="nav_links">
+			<li><a href="/">Главная</a> </li>
+			<li><a href="upload.html">Загрузить</a> </li>
+			<li><a href="#">О нас</a> </li>
+		</ul>
+	</nav>
+
+	<div class="main">
+		<a href="login.html" class="user">Войти</a>
+		<a href="register.html" class="reg">Регистрация</a>
+	</div>
+</header>

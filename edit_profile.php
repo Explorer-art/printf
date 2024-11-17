61a91f525e21f06879abfd35cac5406a084fe851
Den <denstrilko@gmail.com>
2024-11-17 20:12:05 +0300

    fix featch 2

--- edit_profile.php
+++ edit_profile.php
@@ -42,7 +42,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <label for = "username">Имя: </label>
     <input type ="text" id = "username" name ="username" value ="<?php htmlspecialchars($user["username"]);?>" required>
     <label for = "email">Почта: </label>
-    <input type ="text" id = "email" name ="email" value ="<?php htmlspecialchars($email["email"]);?>" required>
+    <input type ="text" id = "email" name ="email" value ="<?php htmlspecialchars($user["email"]);?>" required>
     <button type="submit">Сохранить изминения </button>
 </form>
 <a href = "profile.php">Назад к профилю</a>
+

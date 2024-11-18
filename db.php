be984f04cbb8529fee9f8e1b236b10ca1c4506b0
Truzme_ <56657421+Explorer-art@users.noreply.github.com>
2024-11-19 00:58:49 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 define("HOST", "localhost");
 define("USER", "root");
-define("PASSWORD", "hDRDshWbH5.!dN");
-define("DATABASE", "printf");
+define("PASSWORD", "password");
+define("DATABASE", "db");
 
 try {
     $connection = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE, USER, PASSWORD);

820f859f623d68431b0610e4f8a4a35d644eb0a6
Truzme_ <56657421+Explorer-art@users.noreply.github.com>
2024-11-19 00:48:34 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,8 @@
 <?php
 define("HOST", "localhost");
 define("USER", "root");
-<<<<<<< Updated upstream
 define("PASSWORD", "hDRDshWbH5.!dN");
 define("DATABASE", "registerUsers");
-=======
-define("PASSWORD", "password");
-define("DATABASE", "printf");
->>>>>>> Stashed changes
 
 try {
     $connection = new PDO("mysql:host=" . HOST . ";dbname=" . DATABASE, USER, PASSWORD);

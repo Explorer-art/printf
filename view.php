1ce272a101ece4f3b5b410f1dc3be3a9d60b0a2e
Truzme_ <green57421@gmail.com>
2024-11-19 17:07:05 +0300

    feat: add view.php

---
+++ view.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/static/styles/header_style.css">
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+	<title>Printf</title>
+</head>
+<body>
+	<?php
+		session_start();
+
+		if (isset($_SESSION["user_id"])) {
+			require_once("header_auth.php");
+		} else {
+			require_once("header_unauth.php");
+		}
+	?>
+
+	<br>
+
+	<?php
+		$images_per_page = 10;
+		$page = $_GET["page"];
+
+		require_once("db.php");
+
+		if ($page == 1) {
+			$query = $connection->prepare("SELECT * FROM images ORDER BY created_date DESC LIMIT :images_per_page");
+			$query->bindParam("images_per_page", $images_per_page, PDO::PARAM_STR);
+			$query->execute();
+		} elseif ($paeg == 2) {
+			$query = $connection->prepare("SELECT * FROM images ORDER BY created_date DESC LIMIT :images_per_page OFFSET :images_per_page");
+			$query->bindParam("images_per_page", $images_per_page, PDO::PARAM_STR);
+			$query->execute();
+		} else {
+			$query = $connection->prepare("SELECT * FROM images ORDER BY created_date DESC LIMIT :images_per_page OFFSET :page * :images_per_page");
+			$query->bindParam("images_per_page", $images_per_page, PDO::PARAM_STR);
+			$query->bindParam("page", $page, PDO::PARAM_STR);
+			$query->execute();
+		}
+
+		$images = $query->fetchAll();
+
+		foreach ($images at $image) {
+			echo '<div>';
+			echo '<p>" . htmlspecialchars($image["file_name"]) . "</p>';
+			echo '<img src="' . htmlspecialchars($image["file_path"]) . '">'
+			echo '<div>';
+		}
+	?>
+</body>
+</html>

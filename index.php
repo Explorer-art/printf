44ebfc7a5dbd76a7d9a515cc85ae11d65418faf2
Truzme_ <green57421@gmail.com>
2024-11-19 17:41:10 +0300

    Update

--- index.php
+++ index.php
@@ -10,13 +10,65 @@
 </head>
 <body>
 	<?php
-	session_start();
+		session_start();
 
-	if (isset($_SESSION["user_id"])) {
-		require_once("header_auth.php");
-	} else {
-		require_once("header_unauth.php");
-	}
+		if (isset($_SESSION["user_id"])) {
+			require_once("header_auth.php");
+		} else {
+			require_once("header_unauth.php");
+		}
+	?>
+
+	<br>
+
+	<?php
+		$images_per_page = 10;
+
+		if (isset($_GET["page"])) {
+			$page = $_GET["page"];
+		} else {
+			$page = 1;
+		}
+
+		$query = $connection->query("SELECT COUNT(*) FROM images");
+
+		$total_images = $query->fetchColumn();
+		$total_pages = ceil($total_images / $images_per_page);
+
+		if ($total_images == 0) {
+			echo "Изображения не найдены";
+			exit();
+		}
+
+		if ($page > $total_pages) {
+			$page = $total_pages;
+		}
+
+		require_once("db.php");
+
+		if ($page == 1) {
+			$query = $connection->prepare("SELECT * FROM images ORDER BY created_date DESC LIMIT :images_per_page");
+			$query->bindParam("images_per_page", $images_per_page, PDO::PARAM_INT);
+			$query->execute();
+		} elseif ($page == 2) {
+			$query = $connection->prepare("SELECT * FROM images ORDER BY created_date DESC LIMIT :images_per_page OFFSET :images_per_page");
+			$query->bindParam("images_per_page", $images_per_page, PDO::PARAM_INT);
+			$query->execute();
+		} else {
+			$query = $connection->prepare("SELECT * FROM images ORDER BY created_date DESC LIMIT :images_per_page OFFSET :page * :images_per_page");
+			$query->bindParam("images_per_page", $images_per_page, PDO::PARAM_INT);
+			$query->bindParam("page", $page, PDO::PARAM_STR);
+			$query->execute();
+		}
+
+		$images = $query->fetchAll();
+
+		foreach ($images as $image) {
+			echo '<div>';
+			echo '<p>"' . htmlspecialchars($image["file_name"]) . '"</p>';
+			echo '<img src="' . htmlspecialchars($image["file_path"]) . '" width=250>';
+			echo '<div>';
+		}
 	?>
 </body>
 </html>

049c1505b540f5d0616b7bbf538a1ac2e3cf3dbf
Truzme_ <green57421@gmail.com>
2024-11-19 17:28:26 +0300

    Update view.php

--- view.php
+++ view.php
@@ -61,7 +61,7 @@
 		foreach ($images as $image) {
 			echo '<div>';
 			echo '<p>"' . htmlspecialchars($image["file_name"]) . '"</p>';
-			echo '<img src="' . htmlspecialchars($image["file_path"]) . ' width="250">';
+			echo '<img src="' . htmlspecialchars($image["file_path"]) . '" width=250>';
 			echo '<div>';
 		}
 	?>

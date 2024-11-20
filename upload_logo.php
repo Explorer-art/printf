09fb5d18cef5684203f108a4fd2603bc05cbc852
Den <denstrilko@gmail.com>
2024-11-20 23:59:39 +0300

    fix

--- upload_logo.php
+++ upload_logo.php
@@ -8,17 +8,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (isset($logo_file) and $logo_file['error'] === UPLOAD_ERR_OK) {
         $logo_dir = "/home/webadmin/printf/logo/";
-        $logo_name = basename($logo_file["name"]);
-        $logo_path = $logo_dir . $logo_name;
-        $image_file_type = strtolower(pathinfo($logo_path, PATHINFO_EXTENSION));
+
+        $image_file_type = strtolower(pathinfo($logo_file["name"], PATHINFO_EXTENSION));
+        $unique_name = uniqid('avatar_', true) . '.' . $image_file_type; // добавляем префикс и расширение
+        $logo_path = $logo_dir . $unique_name;
 
         $check = getimagesize($logo_file["tmp_name"]);
         if ($check === false) {
             die("Файл не является изображением.");
-        }
-
-        if (file_exists($logo_path)) {
-            die("Изображение уже существует.");
         }
 
         if ($logo_file["size"] > 1000000) {
@@ -29,10 +26,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             die("Разрешены только файлы JPG, JPEG, PNG и GIF.");
         }
 
-        if (move_uploaded_file($logo_file["tmp_name"], $logo_path))
-        {
+        if (move_uploaded_file($logo_file["tmp_name"], $logo_path)) {
             $query = $connection->prepare("UPDATE users SET logo = ? WHERE id = ?");
-            if ($query->execute(["logo/" . $logo_name, $user_id])) {
+            if ($query->execute(["logo/" . $unique_name, $user_id])) {
                 echo "Аватар успешно обновлен!";
             } else {
                 echo "Ошибка при обновлении аватара.";

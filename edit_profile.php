74557e6258cb7315f79d8cb14b10bc1236e478c7
Den <denstrilko@gmail.com>
2024-11-22 23:07:39 +0300

    feat

--- edit_profile.php
+++ edit_profile.php
@@ -16,6 +16,7 @@ $user = $query->fetch();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $email = trim(strip_tags(htmlspecialchars($_POST["email"])));
+    $description = trim(strip_tags(htmlspecialchars($_POST["description"])));
 
     $emailQuery = $connection->prepare("SELECT * FROM users WHERE email = ? AND id != ?");
     $emailQuery->execute([$email, $user_id]);
@@ -32,69 +33,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
+
     $query = $connection->prepare("UPDATE users SET username = ? WHERE id = ?");
     if ($query->execute([$username, $user_id])) {
-        $data = [
-            "success" => true,
-            "message" => "Имя пользователя успешно обновлено!"
-        ];
-
-        header("Content-Type: application/json; charset=utf-8");
-        http_response_code(200);
-        echo json_encode($data);
+       echo  "Успешно обновлено";
     } else {
-        $data = [
-            "success" => false,
-            "message" => "Ошибка обновления имени пользователя"
-        ];
-
-        header("Content-Type: application/json; charset=utf-8");
-        http_response_code(500);
-        echo json_encode($data);
-        exit();
+        echo "Ошибка обновления имени пользователя";
     }
+
 
     $query = $connection->prepare("UPDATE users SET email = ? WHERE id = ?");
     if ($query->execute([$email, $user_id])) {
-        $data = [
-            "success" => true,
-            "message" => "Электронная почта успешно обновлена!"
-        ];
-
-        header("Content-Type: application/json; charset=utf-8");
-        http_response_code(200);
-        echo json_encode($data);
+        echo "Успешно обновлено";
     } else {
-        $data = [
-            "success" => false,
-            "message" => "Ошибка обновления электронной почты"
-        ];
-
-        header("Content-Type: application/json; charset=utf-8");
-        http_response_code(500);
-        echo json_encode($data);
+         echo "Ошибка обновления электронной почты";
     }
 
-    // Перенаправление на профиль после успешного обновления
-    header("Location: profile.php");
-    exit();
+
+    $query = $connection->prepare("UPDATE users SET description = ? WHERE id = ?");
+    if ($query->execute([$description, $user_id])) {
+        echo  "Успешно обновлено";
+        header("Location: profile.php");
+        exit();
+    } else {
+         echo "Ошибка обновления описания";
+        http_response_code(500);
+        echo json_encode(["success" => false, "message" => "Ошибка обновления описания"]);
+        exit();
+    }
 }
 ?>
 
 <h1>Редактировать профиль</h1>
 
 <form action="upload_logo.php" method="post" enctype="multipart/form-data">
-    <label for="logo"> Выберите аватар: </label>
+    <label for="logo">Выберите аватар:</label>
     <input type="file" name="logo" id="logo" accept="image/*" required>
-    <button type="submit">Сохранить аватар: </button>
+    <button type="submit">Сохранить аватар</button>
 </form>
 
-<form action="edit_profile.php" method="post" >
+<form action="edit_profile.php" method="post">
     <label for="username">Имя:</label>
     <input type="text" id="username" name="username" value="<?php echo htmlspecialchars($user["username"]); ?>">
 
     <label for="email">Новая электронная почта:</label>
     <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($user["email"]); ?>">
+
+    <label for="description">Описание:</label>
+    <textarea id="description" name="description"><?php echo htmlspecialchars($user["description"]); ?></textarea>
 
     <button type="submit">Сохранить изменения</button>
 </form>
